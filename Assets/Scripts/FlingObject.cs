b81321db407fd5833fbf6d30653963ec85dc92a0
Michael Gonzalez <mmg005@ucsd.edu>
2015-06-07 13:14:53 -0700

    Initial commit

---
+++ Assets/Scripts/FlingObject.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlingObject : MonoBehaviour {
+
+    public GameObject obj;
+    public float forceScalar = 1;
+    public float yScalar = 1;
+    public float piScalar;
+    public Vector3 initalVector, finalVector, deltaVector;
+	void Start () {
+	
+	}
+	
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            initalVector = Input.mousePosition;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            finalVector = Input.mousePosition;
+            Fling();
+        }
+	}
+
+    private void Fling()
+    {
+        // Calculate the delta vector
+        deltaVector = finalVector - initalVector;
+        deltaVector *= forceScalar;
+        // We want to know the parent object's y rotation in radians
+        float yDirection = transform.rotation.eulerAngles.y * Mathf.PI / 180;
+        yDirection -= (Mathf.PI * piScalar);
+        float forceMagnitude = deltaVector.magnitude;
+        // To make the vector definition cleaner, temporary variables are used
+        float x = Mathf.Sin(yDirection) * forceMagnitude;
+        float z = Mathf.Cos(yDirection) * forceMagnitude;
+        //yDirection += (Mathf.PI * piScalar);
+        Vector3 forceVector = new Vector3(x, forceMagnitude, z);
+        // Instantiate the game object to be flung
+        GameObject objRuntime = (GameObject)GameObject.Instantiate(obj);
+        objRuntime.transform.position = transform.position;
+        // Add the rigid body to the object if it doesn't exist
+        Rigidbody rigidBody = objRuntime.GetComponent<Rigidbody>();
+        if (rigidBody == null)
+            rigidBody = objRuntime.AddComponent<Rigidbody>();
+        rigidBody.AddForce(forceVector, ForceMode.Impulse);
+    }
+}
---
+++ Assets/Scripts/RandomColorLerper.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomColorLerper : MonoBehaviour {
+
+    public float lerpTime = 1;
+    Color currColor, nextColor;
+    float startTime;
+    Renderer rend;
+	// Use this for initialization
+	void Start () {
+        startTime = -10;
+        nextColor = GetRandomColor();
+        rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time > startTime + lerpTime)
+        {
+            currColor = nextColor;
+            nextColor = GetRandomColor();
+            startTime = Time.time;
+        }
+        else
+        {
+            float lerpVal = (Time.time - startTime)/lerpTime;
+            Color lerpColor = Color.Lerp(currColor, nextColor, lerpVal );
+            rend.material.color = lerpColor;
+        }
+	}
+    
+
+    Color GetRandomColor()
+    {
+        const float MAXRGB = 255;
+        float r, g, b;
+        r = Random.Range(0, MAXRGB)/MAXRGB;
+        g = Random.Range(0, MAXRGB)/MAXRGB;
+        b = Random.Range(0, MAXRGB)/MAXRGB;
+        return new Color(r,g,b);
+    }
+}

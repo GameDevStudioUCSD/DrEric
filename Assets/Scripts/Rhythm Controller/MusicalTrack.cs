54bd58e985a77e1fe117b9dadf7eac2caa35b322
Paula Q <pnquach@ucsd.edu>
2015-10-10 16:42:56 -0700

    MusicalTrack class

---
+++ Assets/Scripts/Rhythm Controller/MusicalTrack.cs
@@ -0,0 +1,36 @@
+﻿/* Header comments here
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class MusicalTrack : MonoBehaviour
+{
+    public int bpm;
+    public int timeSigUpper;
+    public int timeSigLower;
+    public AudioSource song;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void Play()
+    {
+        song.PlayOneShot();
+    }
+
+    void PlayAt(int measure, int beat)
+    {
+        
+        song.PlayOneshot();
+    }
+}

4ae46be07f3c1b1a62fea94acac79e1429d7ee99
Leon Cheung <funarchitect2022@gmail.com>
2015-10-10 15:37:29 -0700

    references to image and text

--- dialog-box-scripts/Dialog.cs
+++ dialog-box-scripts/Dialog.cs
@@ -2,5 +2,14 @@ using UnityEngine;
 using System.Collections;
 
 public class Dialog : MonoBehaviour {
+	private Text text;
+	private Image image;
 	
+	void Start() {
+
+	}
+
+	void Update() {
+
+	}
 }

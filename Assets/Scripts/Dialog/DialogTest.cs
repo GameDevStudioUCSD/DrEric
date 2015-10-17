0e7645b765eb2449a5036ca5f3a683d2ed9060f6
SherZahed <sherzahed@yahoo.com>
2015-10-16 18:16:28 -0700

    Contols prelim testing

--- Assets/Scripts/Dialog/DialogTest.cs
+++ Assets/Scripts/Dialog/DialogTest.cs
@@ -12,6 +12,6 @@ public class DialogTest : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		conversation.getNext ().display ();
+        conversation.getNext ().display ();
 	}
 }

be5ebd07be789e0a7276dba05ce81c07f7e7732a
ericluo05 <eric_luo@Ymail.com>
2015-10-24 16:09:35 -0700

    Switch+Activator

---
+++ Assets/Scripts/Activator.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Activator : MonoBehaviour {
+    /* used jointly with Switch.cs, place switch on a Switcher,
+     * drag all GameObjects activated/deactivated by the switch
+     * into the Switch componoent of the switcher, make sure they
+     * have a Activator component
+     */
+
+
+    // reverseActivate: if true, isActive->!isActive upon every call of Activate()
+    //if false, isActive remain false if it is already false
+    public bool reverseActivate; 
+    //isActive: initially active
+    public bool isActive;
+
+	void Start () {
+        reverseActivate = false;
+        isActive = true;
+    }
+	
+
+    // called when Switch is touched by placer 
+    // put stuff that will happen(on/off) on DOSOMETHING() at the end 
+   public void Activate()
+    {
+        if (reverseActivate)
+        {
+            isActive = !isActive;
+            DoSomething();
+        }
+        else
+        {
+            isActive = false;
+            DoSomething();
+        }
+         
+    }
+
+    
+    private void DoSomething()
+    {
+      //  transform.Translate(new Vector3(1, 1, 0));
+    }
+
+
+}
---
+++ Assets/Scripts/Swith.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Swith : MonoBehaviour {
+    /* used jointly with Switch.cs, place switch on a Switcher,
+    * drag all GameObjects activated/deactivated by the switch
+    * into the Switch componoent of the switcher, make sure they
+    * have a Activator component
+    */
+
+
+    public GameObject[] objectToBeActivated;
+    private Activator[] act;
+
+    // Use this for initialization
+    void Start()
+    {
+        act = new Activator[objectToBeActivated.Length];
+        for (int i = 0; i < objectToBeActivated.Length; i++)
+        {
+           int hash = objectToBeActivated[0].transform.GetHashCode();
+           // Debug.Log("Activator " + i + " hashcode: " + hash);
+            act[i] = (objectToBeActivated[i]).GetComponent<Activator>() as Activator;
+        }
+    }
+
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        //Debug.Log("Activator touched");
+          if (other.gameObject.tag == "Player" )
+          {
+              for (int i = 0; i < act.Length ; i++)
+                  act[i].Activate();
+          }
+    }
+
+
+}

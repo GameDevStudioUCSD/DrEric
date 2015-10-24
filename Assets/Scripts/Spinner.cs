eed141320235d7905a9e18c1b8078bd7ba20785d
Alec Brickner <alecjbrick@hotmail.com>
2015-10-24 15:57:43 -0700

    Add spinner (not done)

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spinner : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.forward, speed);
+	}
+}

d98b66eeb24a98e5b91ea623890e69d0d9642f34
Alec Brickner <alecjbrick@hotmail.com>
2015-10-24 16:29:57 -0700

    Try to make spinner better

--- Assets/Scripts/Spinner.cs
+++ Assets/Scripts/Spinner.cs
@@ -12,6 +12,6 @@ public class Spinner : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(Vector3.forward, speed);
+        GetComponent<Rigidbody2D>().AddTorque(10);
 	}
 }

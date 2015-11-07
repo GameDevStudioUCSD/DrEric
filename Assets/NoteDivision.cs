555dc0d848baa487d996bb7cb3a7ed83e7b36afc
jameslala <james.henry.allen@gmail.com>
2015-11-07 15:28:09 -0800

    Key added

---
+++ Assets/NoteDivision.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Filename: NoteDivision.cs \n
+ * Author: N/A \n
+ * Contributing Authors: Michael Gonzalez\n
+ * Date Drafted: 10/20/2015\n
+ * Description: Represents a specific note length. Ex: Whole Note, Half Note, 
+ * Quarter Note, Whole Note Tripplet, etc.
+ */
+public enum NoteDivision {
+	sixteenthNote,
+	eighthNote,
+	quarterNote,
+	halfNote,
+	wholeNote,
+	tripleWholeNote,
+	tripleHalfNote,
+	tripleQuarterNote,
+	tripleEigthNote
+}
+
---
+++ Assets/Scripts/KeyControllerScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyControllerScript : MonoBehaviour {
+
+    public int keys_collected;
+
+	void Start () {
+        keys_collected = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void incrementKeys()
+    {
+        keys_collected++;
+    }
+
+    public int getKeys()
+    {
+        return keys_collected;
+    }
+        
+}

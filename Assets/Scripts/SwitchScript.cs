eaec0e4ffaa026c23437f89e89e2b5fdfbcdc25c
jameslala <james.henry.allen@gmail.com>
2015-11-07 16:24:20 -0800

    switch base created

---
+++ Assets/Scripts/SwitchScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class SwitchScript : MonoBehaviour {
+
+    public UnityEvent Event;
+	// Use this for initialization
+	void Start () {
+        Event.AddListener(doSomething);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Switch triggered");
+            Event.Invoke();
+        }
+    }
+    void doSomething()
+    {
+        Debug.Log("somethng");
+    }
+}

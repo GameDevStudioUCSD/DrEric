adf1a7cee57d59ddc60af2af9a0b3841e8539a7d
MichaelMGonzalez <mmg005@ucsd.edu>
2015-11-14 16:18:33 -0800

    Improved FlingObject to work in any arbitrary orientation

--- Assets/Scripts/FlingObject.cs
+++ Assets/Scripts/FlingObject.cs
@@ -40,6 +40,7 @@ public class FlingObject : MonoBehaviour {
     /** Awaits a user's mouse clicks; saves their clicks as vectors; and calls 
      *  Fling() when the user releases their mouse */
 	void Update () {
+        //Debug.Log(transform.rotation.eulerAngles.normalized);
 		if (transform.parent == null) {
 			if (Input.GetMouseButtonDown (0)) {
 				initialVector = Input.mousePosition;
@@ -61,8 +62,11 @@ public class FlingObject : MonoBehaviour {
 	}
 	/** Constructs the impulse vector */
 	public Vector2 CalculateDelta(Vector2 initialVector, Vector2 finalVector) {
-		// Calculate the delta vector
+        // Calculate the delta vector
 		deltaVector = finalVector - initialVector;
+        Debug.Log("Original Delta Vector: " +deltaVector);
+        deltaVector.x = -1 * deltaVector.x;
+        deltaVector.y = -1 * deltaVector.y;
 		
 		if (isReverseControl)
 		{
@@ -85,7 +89,14 @@ public class FlingObject : MonoBehaviour {
      *  play a jump sound! */
     public void Fling(Vector2 deltaVector)
     {
-        rigidBody.AddForce(deltaVector, ForceMode2D.Impulse);
+        float rotationAngle = -1 * (transform.rotation.eulerAngles.x ) * Mathf.Deg2Rad;
+        float sinAngle = Mathf.Sin(rotationAngle);
+        float cosAngle = Mathf.Cos(rotationAngle);
+        float rotatedX = ((deltaVector.x * cosAngle) - (deltaVector.y * sinAngle));
+        float rotatedY = ((deltaVector.x * sinAngle) + (deltaVector.y * cosAngle));
+        Vector2 rotatedVector = new Vector2(rotatedX, rotatedY);
+        Debug.Log(rotatedVector);
+        rigidBody.AddForce(rotatedVector, ForceMode2D.Impulse);
         // To make this method more abstract, this has to be moved...
         AudioSource audio = GetComponent<AudioSource>();
         audio.Play();

4ce42974cfbe59d6af8419b3112d4d18490084cd
jameslala <james.henry.allen@gmail.com>
2015-11-23 21:36:51 -0800

    bos finally atks

--- Assets/Scripts/Level_1 Mechanics/Boss1.cs
+++ Assets/Scripts/Level_1 Mechanics/Boss1.cs
@@ -3,37 +3,86 @@ using System.Collections;
 
 public class Boss1 : MonoBehaviour {
     public enum Direction { DOWN, UP, LEFT, RIGHT}
+
 	public bool topSwitch, leftSwitch, rightSwitch, bottomSwitch;
     private bool needToPickASwitch;
+
+    public bool currentlyAttacking;
+
+    private bool currentlyLerping;
+    private bool goingAway;
+    private float startTime;
+    private float endTime;
+    private Direction currentDirection;
+    
 	// Use this for initialization
 	void Start () {
 		topSwitch = false;
 		leftSwitch = false;
 		rightSwitch = false;
 		bottomSwitch = false;
-        needToPickASwitch = false;
+        needToPickASwitch = true;
+        goingAway = false;
+        endTime = 1;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (!needToPickASwitch)
+        if (needToPickASwitch) PickASwitch();
+        if (currentlyLerping) Lerp(currentDirection);
+        if (!currentlyAttacking)
         {
-            PickASwitch();
+            BodySlam(Direction.DOWN);
+        }
+
+        
+    }
+
+    void Lerp(Direction direction)
+    {
+        Vector2 startVector = transform.position;
+        Vector2 endVector = Vector2.down;//default val
+        if (direction == Direction.DOWN)
+        {
+            endVector = transform.position + .13f * Vector3.down;
+        }
+        if (goingAway)
+        {
+            if (Time.time - startTime < endTime)
+            {
+                //Debug.Log(Time.time - startTime);
+                float Lerpval = (Time.time - startTime) / endTime;
+            Debug.Log(Lerpval);
+                transform.position = Vector2.Lerp(startVector, endVector, Lerpval);
+            }
+            else
+            {
+                startTime = Time.time;
+                goingAway = false;
+            }
+        }
+        else if (!goingAway)
+        {
+            if (Time.time - startTime < endTime)
+            {
+                //calculate opposite end vector
+                Vector2 tempV = endVector - startVector;
+                endVector = startVector - tempV;
+                //Debug.Log(Time.time - startTime);
+                float Lerpval = (Time.time - startTime) / endTime;
+                transform.position = Vector2.Lerp(startVector, endVector, Lerpval);
+            }
+            else currentlyLerping = false;
         }
     }
 
 	void BodySlam(Direction direction){
+        currentlyAttacking = true;
+        startTime = Time.time;
         if (direction == Direction.DOWN)
-        {
-            for (int x = 0; x < 100; x++)
-            {
-                transform.position = transform.position + Vector3.down;
-            }
-            for (int x = 0; x < 100; x++)
-            {
-                transform.position = transform.position - Vector3.down;
-            }
-        }
+            currentDirection = Direction.DOWN;
+        currentlyLerping = true;
+        goingAway = true;
 	}
 
 	void Attack2() {
@@ -81,17 +130,16 @@ public class Boss1 : MonoBehaviour {
         ArrayList list = new ArrayList();
         if (!topSwitch) list.Add(transform.FindChild("TopSwitch"));
         if (!bottomSwitch) list.Add(transform.FindChild("BottomSwitch"));
-        if (!rightSwitch) list.Add(transform.FindChild("LeftSwitch"));
-        if (!leftSwitch) list.Add(transform.FindChild("RightSwitch"));
+        if (!rightSwitch) list.Add(transform.FindChild("RightSwitch"));
+        if (!leftSwitch) list.Add(transform.FindChild("LeftSwitch"));
         //disable all the switches
         for (int forloopindex = 0; forloopindex < list.Count; forloopindex++)
         {
             Transform currenttransform = (Transform)list[forloopindex];
             Switch currentswitch =  currenttransform.gameObject.GetComponent<Switch>();
-            Debug.Log(currenttransform.gameObject);
+            //Debug.Log(currenttransform.gameObject);
             currentswitch.isEnabled = false;
         }
-
         //choose a switch at random 
         int size = list.Count;
         int index = Random.Range(0, size);
@@ -102,6 +150,6 @@ public class Boss1 : MonoBehaviour {
         Debug.Log(currentTransform.gameObject + " ENABLED");
         currentSwitch.isEnabled = true;
 
-        needToPickASwitch = true;
+        needToPickASwitch = false;
     }
 }

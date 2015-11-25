3cb7afcb64d723aca27d05d9bcc821f1faa61143
jameslala <james.henry.allen@gmail.com>
2015-11-24 18:11:53 -0800

    boss body slam done

--- Assets/Scripts/Level_1 Mechanics/Boss1.cs
+++ Assets/Scripts/Level_1 Mechanics/Boss1.cs
@@ -3,17 +3,25 @@ using System.Collections;
 
 public class Boss1 : MonoBehaviour {
     public enum Direction { DOWN, UP, LEFT, RIGHT}
+    enum STATE { LERPING_AWAY, LERPING_BACK,IDLE,NOTHING}
 
 	public bool topSwitch, leftSwitch, rightSwitch, bottomSwitch;
+    public float moveFactor = 3f;
+
+
     private bool needToPickASwitch;
+    public string TOPSWITCH = "TopSwitch";
+    public string BOTTOMSWITCH = "BottomSwitch";
+    public string RIGHTSWITCH = "RightSwitch";
+    public string LEFTSWITCH = "LeftSwitch";
 
-    public bool currentlyAttacking;
 
-    private bool currentlyLerping;
-    private bool goingAway;
     private float startTime;
-    private float endTime;
+    private float endTime = 1;
     private Direction currentDirection;
+    private STATE state;
+    private Vector2 startVector = Vector2.down;//default value
+    private Vector2 endVector = Vector2.down;
     
 	// Use this for initialization
 	void Start () {
@@ -22,67 +30,81 @@ public class Boss1 : MonoBehaviour {
 		rightSwitch = false;
 		bottomSwitch = false;
         needToPickASwitch = true;
-        goingAway = false;
-        endTime = 1;
+        state = STATE.IDLE;
+        startVector = transform.position;
+        startTime = Time.time+1;
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (needToPickASwitch) PickASwitch();
-        if (currentlyLerping) Lerp(currentDirection);
-        if (!currentlyAttacking)
+        if (state == STATE.LERPING_BACK || state == STATE.LERPING_AWAY) Lerp(currentDirection);
+        if (state == STATE.IDLE && Time.time - startTime > endTime)
         {
-            BodySlam(Direction.DOWN);
+            BodySlam((Direction)Random.Range(0,3));
         }
-
-        
     }
 
     void Lerp(Direction direction)
     {
-        Vector2 startVector = transform.position;
-        Vector2 endVector = Vector2.down;//default val
-        if (direction == Direction.DOWN)
-        {
-            endVector = transform.position + .13f * Vector3.down;
-        }
-        if (goingAway)
+        if (state == STATE.LERPING_AWAY)
         {
             if (Time.time - startTime < endTime)
             {
                 //Debug.Log(Time.time - startTime);
                 float Lerpval = (Time.time - startTime) / endTime;
-            Debug.Log(Lerpval);
+                //Debug.Log(Lerpval);
                 transform.position = Vector2.Lerp(startVector, endVector, Lerpval);
             }
             else
             {
                 startTime = Time.time;
-                goingAway = false;
+                state = STATE.LERPING_BACK;
             }
         }
-        else if (!goingAway)
+        else if (state == STATE.LERPING_BACK)
         {
             if (Time.time - startTime < endTime)
             {
-                //calculate opposite end vector
-                Vector2 tempV = endVector - startVector;
-                endVector = startVector - tempV;
                 //Debug.Log(Time.time - startTime);
                 float Lerpval = (Time.time - startTime) / endTime;
-                transform.position = Vector2.Lerp(startVector, endVector, Lerpval);
+                transform.position = Vector2.Lerp(endVector, startVector, Lerpval);
             }
-            else currentlyLerping = false;
+            else
+            {
+                startTime = Time.time;
+                state = STATE.IDLE;
+            }
         }
     }
 
 	void BodySlam(Direction direction){
-        currentlyAttacking = true;
         startTime = Time.time;
+        Vector2 tempVector = Vector2.down;
         if (direction == Direction.DOWN)
+        {
             currentDirection = Direction.DOWN;
-        currentlyLerping = true;
-        goingAway = true;
+            tempVector = Vector2.down * moveFactor;
+        }
+        if (direction == Direction.UP)
+        {
+            currentDirection = Direction.UP;
+            tempVector = Vector2.up * moveFactor;
+        }
+        if (direction == Direction.LEFT)
+        {
+            currentDirection = Direction.LEFT;
+            tempVector = Vector2.left * moveFactor;
+        }
+        if (direction == Direction.RIGHT)
+        {
+            currentDirection = Direction.RIGHT;
+            tempVector = Vector2.right * moveFactor;
+        }
+
+        endVector = startVector + tempVector;
+        state = STATE.LERPING_AWAY;
+
 	}
 
 	void Attack2() {
@@ -91,25 +113,25 @@ public class Boss1 : MonoBehaviour {
 
 	public void FlipRightSwitch(){
 		rightSwitch = true;
-        Switch currentswitch = transform.FindChild("RightSwitch").gameObject.GetComponent<Switch>();
+        Switch currentswitch = transform.FindChild(RIGHTSWITCH).gameObject.GetComponent<Switch>();
         currentswitch.isEnabled = false;
         PickASwitch();
     }
 	public void FlipTopSwitch(){
 		topSwitch = true;
-        Switch currentswitch = transform.FindChild("TopSwitch").gameObject.GetComponent<Switch>();
+        Switch currentswitch = transform.FindChild(TOPSWITCH).gameObject.GetComponent<Switch>();
         currentswitch.isEnabled = false;
         PickASwitch();
     }
 	public void FlipLeftSwitch(){
 		leftSwitch = true;
-        Switch currentswitch = transform.FindChild("LeftSwitch").gameObject.GetComponent<Switch>();
+        Switch currentswitch = transform.FindChild(LEFTSWITCH).gameObject.GetComponent<Switch>();
         currentswitch.isEnabled = false;
         PickASwitch();
     }
 	public void FlipBottomSwitch(){
 		bottomSwitch = true;
-        Switch currentswitch = transform.FindChild("BottomSwitch").gameObject.GetComponent<Switch>();
+        Switch currentswitch = transform.FindChild(BOTTOMSWITCH).gameObject.GetComponent<Switch>();
         currentswitch.isEnabled = false;
         PickASwitch();
     }
@@ -128,10 +150,10 @@ public class Boss1 : MonoBehaviour {
         }
         //assign all unpressed switches to list
         ArrayList list = new ArrayList();
-        if (!topSwitch) list.Add(transform.FindChild("TopSwitch"));
-        if (!bottomSwitch) list.Add(transform.FindChild("BottomSwitch"));
-        if (!rightSwitch) list.Add(transform.FindChild("RightSwitch"));
-        if (!leftSwitch) list.Add(transform.FindChild("LeftSwitch"));
+        if (!topSwitch) list.Add(transform.FindChild(TOPSWITCH));
+        if (!bottomSwitch) list.Add(transform.FindChild(BOTTOMSWITCH));
+        if (!rightSwitch) list.Add(transform.FindChild(RIGHTSWITCH));
+        if (!leftSwitch) list.Add(transform.FindChild(LEFTSWITCH));
         //disable all the switches
         for (int forloopindex = 0; forloopindex < list.Count; forloopindex++)
         {

581ab0b6c7c9a08385252ba2f49cba2b8c811926
AnhQuan Nguyen <j4qfrost@gmail.com>
2016-01-08 19:51:41 -0800

    game pauses - AQ

---
+++ Assets/ChaosControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChaosControl : MonoBehaviour {
+
+	public void StopTime()
+	{
+		Time.timeScale = 0f;
+	}
+
+	public void StartTime()
+	{
+		Time.timeScale = 1f;
+	}
+}

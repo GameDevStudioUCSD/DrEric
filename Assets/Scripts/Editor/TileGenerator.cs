b56557881637e03f62497ff5ee54847c0a93bd5e
AnhQuan Nguyen <j4qfrost@gmail.com>
2016-01-08 17:41:45 -0800

    meta data

---
+++ Assets/Scripts/Editor/TileGenerator.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class TileGenerator : MonoBehaviour {
+
+
+	[MenuItem("CONTEXT/Wall/GenerateTiles")]
+	static void GenerateTiles() {
+		Transform t = Selection.activeTransform;
+		Wall wall = t.gameObject.GetComponent<Wall> ();
+		bool isVertical = wall.isVertical;
+		int i = wall.numTiles;
+		Sprite sprite = wall.sprite;
+		for(;i > 0; i--)
+		{
+			GameObject go = new GameObject("Tile" + i);
+			go.AddComponent<SpriteRenderer>();
+			go.AddComponent<BoxCollider2D>();
+			go.GetComponent<SpriteRenderer>().sprite = sprite;
+			go.GetComponent<BoxCollider2D>().size = go.GetComponent<SpriteRenderer>().bounds.size;
+			go.transform.parent = t;
+			go.transform.localPosition = isVertical? new Vector2(0,i*0.13f):new Vector2(i*0.16f,0);
+		}
+	}
+
+	[MenuItem("CONTEXT/Wall/DeleteTiles")]
+	static void DeleteTiles() {
+		Transform parent = Selection.activeTransform;
+		int count = parent.childCount;
+		for (; count > 0; count--)
+		{
+			Transform child = parent.GetChild(count-1);
+			if (child.name.Substring(0,4) == "Tile")
+				DestroyImmediate(child.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour {
+
+	public Sprite sprite;
+	public bool isVertical;
+	public int numTiles;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

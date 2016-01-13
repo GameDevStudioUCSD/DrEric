e2e6e02aa47de89df61caf0baaf2bed1b9535f25
AnhQuan Nguyen <j4qfrost@gmail.com>
2016-01-13 14:44:23 -0800

    modified tiling script for efficient box collider

--- Assets/Scripts/Editor/TileGenerator.cs
+++ Assets/Scripts/Editor/TileGenerator.cs
@@ -13,15 +13,19 @@ public class TileGenerator : MonoBehaviour {
 		bool isVertical = wall.isVertical;
 		int i = wall.numTiles;
 		Sprite sprite = wall.sprite;
+		Vector2 spriteSize = sprite.bounds.size;
+		t.gameObject.AddComponent<BoxCollider2D> ();
+		t.gameObject.GetComponent<BoxCollider2D> ().size
+			= isVertical? new Vector2(spriteSize.x,spriteSize.y*i):new Vector2(spriteSize.x*i,spriteSize.y);
+		//t.gameObject.GetComponent<BoxCollider2D>().
+
 		for(;i > 0; i--)
 		{
 			GameObject go = new GameObject("Tile" + i);
 			go.AddComponent<SpriteRenderer>();
-			go.AddComponent<BoxCollider2D>();
 			go.GetComponent<SpriteRenderer>().sprite = sprite;
-			go.GetComponent<BoxCollider2D>().size = go.GetComponent<SpriteRenderer>().bounds.size;
 			go.transform.parent = t;
-			go.transform.localPosition = isVertical? new Vector2(0,i*1.0f):new Vector2(i*1.0f,0);
+			go.transform.localPosition = isVertical? new Vector2(0,(i-2)*1.0f):new Vector2((i-2)*1.0f,0);
 		}
 	}
 
@@ -35,5 +39,6 @@ public class TileGenerator : MonoBehaviour {
 			if (child.name.Substring(0,4) == "Tile")
 				DestroyImmediate(child.gameObject);
 		}
+		DestroyImmediate(parent.GetComponent<BoxCollider2D>());
 	}
 }

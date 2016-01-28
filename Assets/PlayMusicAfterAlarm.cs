901f99ff29cf82169db48c202e4eb90d5a9120db
MichaelMGonzalez <mmg005@ucsd.edu>
2016-01-28 11:33:01 -0800

    boss

---
+++ Assets/PlayMusicAfterAlarm.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayMusicAfterAlarm : MonoBehaviour {
+
+	// Use this for initialization
+    AudioSource audioSource;
+    RhythmController rhythmController;
+    float alarmLength;
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+        alarmLength = audioSource.clip.length;
+        rhythmController = RhythmController.GetController();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time > alarmLength) {
+            rhythmController.PlaySong();
+            enabled = false;
+        }
+        else
+            rhythmController.StopSong();
+
+	}
+}

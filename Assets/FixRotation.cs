cded90bbbea3c4b64672caa89022d065eda9b85c
AnhQuan Nguyen <j4qfrost@gmail.com>
2016-01-30 14:01:49 -0800

    stuff 7

--- Assets/FixRotation.cs
+++ Assets/FixRotation.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class FixRotation : MonoBehaviour {
 
-    Quaternion rotation;
-    void Awake()
-    {
-        rotation = transform.rotation;
-    }
-    void LateUpdate()
-    {
-        transform.rotation = rotation;
-    }
+	public GameObject go;
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.transform.tag == "Player")
+			Rotate ();
+	}
+
+	void Rotate() {
+		Quaternion from = Quaternion.Euler (0, 0, 1);
+		Quaternion to = Quaternion.Euler (0, 0, -1);
+		go.transform.rotation = Quaternion.Lerp(from, to, 1.0f);
+
+	}
+
 }

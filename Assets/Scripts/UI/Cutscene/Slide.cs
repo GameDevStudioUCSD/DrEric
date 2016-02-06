e006eea2e70fbb9577722f8208f2fbce5c612fdd
MichaelMGonzalez <mmg005@ucsd.edu>
2016-02-06 10:40:19 -0800

    Basic framework for cutscene created

---
+++ Assets/Scripts/UI/Cutscene/Slide.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Slide : MonoBehaviour {
+
+    public float length;
+    public Slide[] subSlides;
+
+    private Image[] imagesInSlide;
+
+    // Built in Unity methods 
+	void Start () {
+        DetermineImagesInSlide();
+        SetupFading();
+	}
+	void Update () {
+	
+	}
+
+    // Public methods
+    public Image GetImage()
+    {
+        return gameObject.GetComponent<Image>();
+    }
+    public void SetLength(float l)
+    {
+        length = l;
+        SetupFading();
+    }
+
+    // Private methods
+    private void DetermineImagesInSlide()
+    {
+        imagesInSlide = new Image[1];
+        imagesInSlide[0] = GetImage();
+    }
+
+    private void SetupFading()
+    {
+        FadeUI fadingScript = this.gameObject.AddComponent<FadeUI>();
+        fadingScript.lengthBeforeFade = .9f * length;
+        fadingScript.lengthOfFade = .1f * length;
+        fadingScript.elementsToFade = imagesInSlide; 
+    }
+}

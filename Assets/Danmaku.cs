14f737050a745537f10d7547ace3d9ed863eea02
MichaelMGonzalez <mmg005@ucsd.edu>
2016-02-14 16:22:01 -0800

    made danmaku

---
+++ Assets/Danmaku.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Danmaku : MonoBehaviour {
+
+    public int numberOfBullets = 2;
+    public float distanceFromCenter = 5f;
+    public GameObject bullet;
+    public float circleTime = 2;
+    public float returnTime = 1;
+    public float maxDistance = 1000;
+
+
+    enum State { Circling, Returning }
+    State state = State.Circling;
+    List<PIDController> pidControllers;
+    List<GameObject> bulletInstances;
+    float stateChangeTime = 0f;
+    float waitTime;
+
+	void Start () {
+        waitTime = circleTime;
+        Spawn();
+        
+	}
+	
+	void Update () {
+        if (Time.time - stateChangeTime > waitTime)
+        {
+            switch (state)
+            {
+                case State.Circling:
+                    state = State.Returning;
+                    waitTime = returnTime;
+                    foreach (PIDController pid in pidControllers)
+                        pid.trackingType = PIDController.TrackingType.Vector;
+                    break;
+                case State.Returning:
+                    state = State.Circling;
+                    waitTime = circleTime;
+                    foreach (PIDController pid in pidControllers)
+                        pid.trackingType = PIDController.TrackingType.Transform;
+                    break;
+            }
+            stateChangeTime = Time.time;
+            if(bulletInstances[0].GetComponent<Transform>().position.magnitude > maxDistance)
+            {
+                foreach (GameObject bullet in bulletInstances)
+                {
+                    bullet.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                    bullet.GetComponent<Transform>().position = transform.position;
+                }
+            }
+        }
+	}
+
+    void Spawn()
+    {
+        pidControllers = new List<PIDController>();
+        Vector2 defaultPlacementVector = distanceFromCenter * Vector2.up;
+        bulletInstances = new List<GameObject>();
+        for (int i = 0; i < numberOfBullets; i++)
+        {
+            Vector3 placementVector = VectorFunctions.RotateVector(defaultPlacementVector, i * 360 / numberOfBullets);
+            placementVector += transform.position;
+            bulletInstances.Add((GameObject)GameObject.Instantiate(bullet, transform.position, Quaternion.identity));
+            PIDController pid = bulletInstances[i].GetComponent<PIDController>();
+            pidControllers.Add(pid);
+            pid.destinationVector = placementVector;
+        }
+        for (int i = 0; i < numberOfBullets; i++)
+        {
+            PIDController pid = bulletInstances[i].GetComponent<PIDController>();
+            if (i == numberOfBullets - 1)
+                pid.destinationTransform = bulletInstances[0].GetComponent<Transform>();
+            else
+                pid.destinationTransform = bulletInstances[i + 1].GetComponent<Transform>();
+        }
+    }
+}
---
+++ Assets/Scripts/General Mechanics/VectorFunctions.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VectorFunctions  {
+    public static Vector2 RotateVector(Vector2 vector, float degrees)
+    {
+        float rads = Mathf.Deg2Rad * degrees;
+        Vector2 row1 = new Vector2(Mathf.Cos(rads), -1.0f * Mathf.Sin(rads)); 
+        Vector2 row2 = new Vector2(Mathf.Sin(rads), -1.0f * Mathf.Cos(rads));
+        float x = Vector2.Dot(row1, vector);
+        float y = Vector2.Dot(row2, vector);
+        return new Vector2(x,y);
+    }
+}

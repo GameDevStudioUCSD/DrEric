2937e1c43c3e3e77a39ac0f3184eb3dcf14d2dfa
jameslala <james.henry.allen@gmail.com>
2016-02-20 14:14:54 -0800

    fix explosion fixed

--- Assets/ExplodingCollider.cs
+++ Assets/ExplodingCollider.cs
@@ -4,24 +4,26 @@ using System.Collections;
 public class ExplodingCollider : Chaser {
 
     public float explodingmultiplier = 1;
+    public bool dies = false;
 
     //PIDController pid;
     // Use this for initialization
-    void newStart ()
+    void Start ()
     {
         //pid = GetComponent<PIDController>();
         base.Start();
     }
 	
 	// Update is called once per frame
-	void newUpdate () {
+	void Update () {
         base.Update();
 	}
 
-    void newOnCollisionEnter2D(Collision2D c)
+    void OnCollisionEnter2D(Collision2D c)
     {
-        Debug.Log(gameObject);
+        //Debug.Log(gameObject);
         //base.OnCollisionEnter2D(c);
+        base.OnCollisionEnter2D(c);
         Transform otherTrans = c.gameObject.GetComponent<Transform>();
         if (otherTrans != base.pid.destinationTransform)
         {
@@ -32,6 +34,6 @@ public class ExplodingCollider : Chaser {
         Vector3 drEricImpluse = restDistance * (otherPos - transform.position).normalized;
         c.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(drEricImpluse.x * explodingmultiplier, drEricImpluse.y * explodingmultiplier), ForceMode2D.Impulse);
         
-        Destroy(this.gameObject);
+        if (dies) Destroy(this.gameObject);
     }
 }

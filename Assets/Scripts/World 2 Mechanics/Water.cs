b77edfc03d37f16cacc1f2f629d62460a65e92f8
Steven Lee <ssl024@ucsd.edu>
2016-02-20 16:53:00 -0800

    Changed water
    Water will now only reset jumps if you get back to the surface.
    Jumping out of the water should now decrement jumps properly.

--- Assets/Scripts/World 2 Mechanics/Water.cs
+++ Assets/Scripts/World 2 Mechanics/Water.cs
@@ -17,6 +17,9 @@ using System.Collections;
 
 public class Water : MonoBehaviour {
 
+    // Tag the player object will have
+    static string PLAYER_TAG = "Player";
+
     // Rate at which water will drain
     public float drainRate = 0.1f;
 
@@ -25,6 +28,9 @@ public class Water : MonoBehaviour {
 
     // When true, begins to drain water
     public bool drainingWater = false;
+
+    // When true, gives the player infinite jumps
+    public bool resetsJumps = true;
 
     // Start position of the water
     private Vector2 startPosition;
@@ -53,8 +59,18 @@ public class Water : MonoBehaviour {
 	}
 
     // Called every frame collider stays in the water
-    void OnTriggerStay2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
+        // If the collider is player's, then give player infinite jump
+        if (other.tag == PLAYER_TAG)
+        {
+            if (resetsJumps)
+            {
+                Debug.Log("Reset jumps!");
+                // Land player to reset jumps
+                other.gameObject.GetComponent<BallController>().Land();
+            }
+        }
     }
 
     // Starts the water draining process

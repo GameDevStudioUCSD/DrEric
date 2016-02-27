663ee62ddb7d42098c556a3f933bf657c04bc4e3
Eric Cheng <ericcheng95@gmail.com>
2016-02-26 20:37:28 -0800

    added killable fish

--- Assets/Scripts/World 2 Mechanics/FishSchool.cs
+++ Assets/Scripts/World 2 Mechanics/FishSchool.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class FishSchool : MonoBehaviour {
 
+	public int numFish;
+	public GameObject barrier;
+	public GameObject[] backgrounds;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -11,5 +15,25 @@ public class FishSchool : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void KillAFish()
+	{
+		numFish--;
+		if (numFish == 0)
+		{
+			backgrounds[2].SetActive(false);
+			Destroy(barrier);
+		}
+		else if (numFish < 2)
+		{
+			backgrounds[1].SetActive(false);
+			backgrounds[2].SetActive(true);
+		}
+		else if (numFish < 4)
+		{
+			backgrounds[0].SetActive(false);
+			backgrounds[1].SetActive(true);
+		}
 	}
 }

38b127a68d4f892a3ad6ad434e95735ce324b88e
MichaelMGonzalez <mmg005@ucsd.edu>
2016-02-27 14:56:40 -0800

    New dialog region implemented in Level 5

--- Assets/Scripts/Dialog/Dialog Causers/DialogOnEnter.cs
+++ Assets/Scripts/Dialog/Dialog Causers/DialogOnEnter.cs
@@ -2,14 +2,20 @@
 using System.Collections;
 
 public class DialogOnEnter : MonoBehaviour {
-public string dialog;
-public DialogBox boxText;
-	void OnTriggerEnter2D(Collider2D collider)
+    public string dialog;
+    public DialogBox boxText;
+    public bool appendText;
+    public bool destroyOnEnter = true;
+    void OnTriggerEnter2D(Collider2D collider)
     {
         if(collider.tag == "Player")
         {
-            boxText.DisplayText(dialog);
-            GameObject.Destroy(this.gameObject);
+            if (appendText)
+                boxText.AppendText(dialog);
+            else
+                boxText.DisplayText(dialog);
+            if(destroyOnEnter)
+                GameObject.Destroy(this.gameObject);
         }
     }
 }

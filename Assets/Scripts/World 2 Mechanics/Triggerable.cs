6aac109865847932332e8c824532b49d6869a8a3
Steven Lee <ssl024@ucsd.edu>
2016-04-01 21:17:46 -0700

    Currently fixing Waterfall buttons, not done

--- Assets/Scripts/World 2 Mechanics/Triggerable.cs
+++ Assets/Scripts/World 2 Mechanics/Triggerable.cs
@@ -12,58 +12,7 @@ using System.Collections;
 /// 
 /// </summary>
 
-public class Triggerable : MonoBehaviour {
-
-    // What should the obstacle's starting position be?
-    public bool startAtLoweredPosition = true;
-
-    // The end positions of raising or lowering
-    public Vector2 raisedPosition;
-    public Vector2 loweredPosition;
-
-    // Rate at which obstacle should lerp
-    public float moveRate = 0.1f;
-
-    // States this obstacle can be in
-    enum State { Initial, Raised, Lowered, Raising, Lowering };
-
-    // The current state the obstacle is in
-    private State currentState = State.Initial;
-
-    // The position the obstacle is currently at if it is going from lowering to raising or vice versa
-    private Vector2 intermediatePosition;
-
-    // Used by lerp to determine how much the object has moved
-    private float positionFraction = 0.0f;
-
-	// Use this for initialization
-	void Start () {
-	    // Initialize state based on whether the obstacle starts at lowered or raised position
-        currentState = startAtLoweredPosition ? State.Lowered : State.Raised;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-        // Do something based on what state the obstacle is in
-        switch(currentState)
-        {
-            case State.Raised:
-                break;
-
-            case State.Lowered:
-                break;
-
-            case State.Raising:
-                Raise();
-                break;
-
-            case State.Lowering:
-                Lower();
-                break;
-        }
-	
-	}
+public abstract class Triggerable : MonoBehaviour {
 
     /*
      * Trigger
@@ -71,48 +20,5 @@ public class Triggerable : MonoBehaviour {
      * Access function for obstacle.  Button will have to know about this function
      * to cause the obstacle to lower/raise.
      */
-    public void Trigger()
-    {
-        // If the obstacle is currently in raised position or rising
-        if(currentState == State.Raised || currentState == State.Raising)
-        {
-            currentState = State.Lowering;
-        }
-        // If the obstacle is currently in lowered position or lowering
-        else if(currentState == State.Lowered || currentState == State.Lowering)
-        {
-            currentState = State.Raising;
-        }
-
-        // Get the intermediate position so lerp doesn't jump around
-        intermediatePosition = this.transform.position;
-        positionFraction = 0.0f;
-
-    }
-
-    private void Raise()
-    {
-        if (positionFraction < 1.0)
-        {
-            positionFraction += Time.deltaTime * moveRate;
-            this.transform.position = Vector2.Lerp(intermediatePosition, raisedPosition, positionFraction);
-        }
-        else
-        {
-            currentState = State.Raised;
-        }
-    }
-
-    private void Lower()
-    {
-        if (positionFraction < 1.0)
-        {
-            positionFraction += Time.deltaTime * moveRate;
-            this.transform.position = Vector2.Lerp(intermediatePosition, loweredPosition, positionFraction);
-        }
-        else
-        {
-            currentState = State.Lowered;
-        }
-    }
+    public abstract void Trigger();
 }

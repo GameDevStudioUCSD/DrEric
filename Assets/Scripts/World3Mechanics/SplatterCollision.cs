e8144be7c478eb97cfeda79c97db87b3357ae6b6
Steven Lee <ssl024@ucsd.edu>
2016-05-06 19:27:16 -0700

    Add splatter shader.
    Splatter assets from some guy off the web.

---
+++ Assets/Scripts/World3Mechanics/SplatterCollision.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplatterCollision : MonoBehaviour {
+
+
+    public GameObject splatter;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        // Where the collision happened
+        ContactPoint2D contactPoint = other.contacts[0];
+
+        // TODO: Instantiate splatter
+    }
+}

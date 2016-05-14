52ab58db6cb78c337812505648ec32108d5fa68e
Alec Brickner <alecjbrick@hotmail.com>
2016-05-13 22:37:47 -0700

    Start mother squid

---
+++ Assets/MotherSquid.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MotherSquid : MonoBehaviour {
+
+	public List<Eye> eyes;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void checkIfOpen() {
+		foreach (Eye e in eyes) {
+			if (e.isOpen ())
+				return;
+		}
+		Debug.Log ("ded");
+	}
+}
+		

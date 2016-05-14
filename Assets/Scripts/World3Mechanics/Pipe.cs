eb89ba4ed44fb0e73337a13322cc6b5ccf6696ef
Ajeya Rengarajan <ajrengar@ucsd.edu>
2016-05-13 21:58:43 -0700

    Working on pipes

---
+++ Assets/Scripts/World3Mechanics/Pipe.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pipe : MonoBehaviour {
+	
+	public const int UP = 0;
+	public const int DOWN = 1;
+	public const int LEFT = 2;
+	public const int RIGHT = 3;
+	public const int OPENINGS = 4;
+
+	private Sprite sprite;
+	private SpriteRenderer spriterenderer;
+
+	// valid directions in the pipe
+	// dir[LEFT] == 1 if the pipe has a left dir
+	int [] dir;
+
+	// direction in which the pipe is entered
+	int enterDir;
+
+	// directions in which you leave this pipe
+	// parallel array to dir, if exit[DOWN] == 1
+	// you can exit in this direction
+	int [] exit; 
+
+	// number of openings the pipe has
+	int numExits;
+
+	// Use this for initialization
+	void Start () {
+		spriterenderer = GetComponent <SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+		
+
+	public Pipe (int [] dir, int enterDir, int [] exit)
+	{
+		this.dir = dir;
+		this.enterDir = enterDir;
+		Start ();
+	}
+
+	// returns a random pipe with certain number of openings
+	// and a certain directions
+	public Pipe (int enterDir)
+	{
+
+		dir = new int [OPENINGS];
+		dir [enterDir] = 1;
+		this.enterDir = enterDir;
+
+		// 1/2/3 exits
+		numExits = (int)Random.Range (1, OPENINGS);
+
+		exit = new int[OPENINGS];
+		setExitDirections ();
+		Start ();
+	}
+
+	// sets random directions to the exit
+	public void setExitDirections ()
+	{
+		int index = 0;
+		int randomDir;
+		while (index < numExits) {
+			randomDir = Random.Range (0, 4);
+			if (dir [randomDir] != 1) {
+				dir [randomDir] = 1;
+				exit [randomDir] = 1;
+				index++;
+			}
+			else {
+				continue;
+			}
+		}
+
+	}
+
+
+	public void setSprite ()
+	{
+		// sets sprite based on number of openings and direction
+	}
+}
---
+++ Assets/Scripts/World3Mechanics/PipePath.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PipePath : MonoBehaviour {
+	
+	public const int UP = 0;
+	public const int DOWN = 1;
+	public const int LEFT = 2;
+	public const int RIGHT = 3;
+	public const int OPENINGS = 4;
+
+
+	public Sprite straight;
+	public Sprite bend;
+	public Sprite threeWay;
+	public Sprite fourWay;
+
+	// desired max height and width of the grid
+	public int height;
+	public int width;
+
+	// the current height and width of the grid
+	private int currentWidth, currentHeight;
+
+	Pipe startPipe;
+	int [] startPipeDir = new int[OPENINGS];
+	int [] startPipeExit = new int[OPENINGS];
+	int startPipeEnter;
+
+	// Use this for initialization
+	void Start () {
+
+		// initializing the first pipe
+		startPipeDir [LEFT] = 1;
+		startPipeDir [RIGHT] = 1;
+		startPipeExit [RIGHT] = 1;
+		startPipeEnter = LEFT;
+
+		startPipe = new Pipe (startPipeDir, startPipeEnter, startPipeExit);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	// places a pipe on the grid
+	// place is the pipe to be places
+	// connect is the pipe touching this pipe
+	private void placePipe (Pipe place, Pipe connect)
+	{
+	}
+}
+

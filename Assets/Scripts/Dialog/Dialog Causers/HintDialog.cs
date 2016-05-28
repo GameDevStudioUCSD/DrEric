718ddd1a9976d93f02fba19c23ad92c2e94b29e5
Kamashamasay <tmctigue@ucsd.edu>
2016-05-28 16:37:21 -0700

    Added a Hint UI thing and made it so that it works in Level 1

---
+++ Assets/Scripts/Dialog/Dialog Causers/HintDialog.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HintDialog : MonoBehaviour {
+	public string dialog;
+	public DialogBox boxText;
+	public Sprite characterImage;
+	// Use this for initialization
+	public void calledDialog()
+	{
+		
+		boxText.DisplayText(dialog);
+		if (characterImage != null)
+			boxText.SetImage(characterImage);
+	}
+}
